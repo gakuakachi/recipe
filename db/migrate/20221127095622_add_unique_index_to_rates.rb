95e60d6db1f45f0d9e0de2a84859bf2dcb1d4225
gakuakachi <akatigaku@gmail.com>
2022-11-28 01:07:35 +0900

    add: unique index

--- db/migrate/20221127095622_add_unique_index_to_rates.rb
+++ db/migrate/20221127095622_add_unique_index_to_rates.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddUniqueIndexToRates < ActiveRecord::Migration[7.0]
   def change
-    add_index :rates, [:user_id, :recipe_id], unique: true
+    add_index :rates, %i[user_id recipe_id], unique: true
   end
 end
