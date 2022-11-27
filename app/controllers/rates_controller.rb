9ee4ba72f2ee07037e968babca3551c77eb612a6
gakuakachi <akatigaku@gmail.com>
2022-11-27 18:14:10 +0900

    fix: recipe controllers methods

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -3,11 +3,17 @@
 class RatesController < ApplicationController
   before_action :authenticate
   before_action :set_recipe
+  before_action :check_recipe
   before_action :set_rate, only: %i[update destroy]
   before_action :set_rate_by_recipe_and_user, only: [:create]
-  before_action :rate?, only: %i[update destroy]
+  before_action :check_rate, only: %i[update destroy]
 
   def create
+    if @rate.present?
+      head :bad_request
+      return
+    end
+
     @rate = Rate.new(rate_params)
     if @rate.save
       render json: { rate: @rate }, status: :created
@@ -29,24 +35,24 @@ class RatesController < ApplicationController
   private
 
   def set_recipe
-    @recipe = Recipe.where(uuid: params[:recipe_id], user: current_user).take
+    @recipe = Recipe.find_by(uuid: params[:recipe_id], user: current_user)
+  end
+
+  def check_recipe
     return unless @recipe.blank?
 
     head :not_found
   end
 
   def set_rate
-    @rate = Rate.where(uuid: params[:id], user: current_user).take
+    @rate = Rate.find_by(uuid: params[:id], user: current_user)
   end
 
   def set_rate_by_recipe_and_user
-    @rate = Rate.where(recipe: @recipe, user: current_user).take
-    return unless @rate.present?
-
-    head :bad_request
+    @rate = Rate.find_by(recipe: @recipe, user: current_user)
   end
 
-  def rate?
+  def check_rate
     return unless @rate.blank?
 
     head :not_found
